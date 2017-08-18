4df74adff50d675fab1b7bea15dc68e84952279e
wanglei288 <wanglei59@jd.com>
2017-08-18 17:02:26 +0800

    add photos

--- album/tool.py
+++ album/tool.py
@@ -150,7 +150,7 @@ def cut_photo():
             print_help()
             for infile in file_list:
                 img = Image.open(src_dir+infile)
-                Graphics(infile=src_dir+infile, outfile=src_dir + infile).cut_by_ratio()            
+                Graphics(infile=src_dir+infile, outfile=src_dir + infile).resize_by_size(1024)
         else:
             pass
     else:
@@ -170,7 +170,7 @@ def git_operation():
     os.system('git push origin master')
 
 if __name__ == "__main__":
-    # cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
-    # compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
-    # git_operation()    # 提交到github仓库
+    cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
+    compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
+    git_operation()    # 提交到github仓库
     handle_photo()     # 将文件处理成json格式，存到博客仓库中
